80231f4a981a66c7e2c39f8e619b6d2917bc96bf
adeeb <asalhani@outlook.com>
2019-08-12 08:01:32 +0300

    use IoC

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
+    @Autowired
+    private  LoginService service;
+
     @RequestMapping("/login")
     public String loginMessage(){
-        return  "login ";
+
+        return  service.getLoginMessage();
+    }
+}
+
+@Component
+class LoginService{
+    public String getLoginMessage(){
+        return "logn message from service....";
     }
 }
