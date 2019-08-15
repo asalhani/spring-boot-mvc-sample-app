14c9aba987a381d45bc8c7fdba5211cffc432889
adeeb <asalhani@outlook.com>
2019-08-15 22:44:31 +0300

    read value from config file

--- src/main/java/com/example/controllers/LoginController.java
+++ src/main/java/com/example/controllers/LoginController.java
@@ -1,12 +1,18 @@
 package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.services.LoginService;
 
 @RestController
 public class LoginController {
+
+    @Value("${admin.name}")
+    private String AdminName;
 
     @Autowired
     private LoginService service;
@@ -15,6 +21,11 @@ public class LoginController {
     public String loginMessage(){
 
         return  service.getLoginMessage();
+    }
+
+    @GetMapping("/login/admin")
+    public ResponseEntity<?> GetAdminInfo(){
+        return ResponseEntity.ok(AdminName);
     }
 }
 
