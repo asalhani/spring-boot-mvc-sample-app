3343392e35a9f1745d168857807e1435451e0c1a
adeeb <asalhani@outlook.com>
2019-08-16 23:55:20 +0300

    setup basic auth with authorization

---
+++ src/main/java/com/example/configurations/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("{noop}secret1").roles("USER")
+                .and().withUser("admin1").password("{noop}secret1")
+                .roles("ADMIN", "USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers("/surveys/**").hasRole("USER")
+                .antMatchers("/users/**").hasRole("USER")
+                .antMatchers("/**").hasRole("ADMIN")
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+    }
+
+}
