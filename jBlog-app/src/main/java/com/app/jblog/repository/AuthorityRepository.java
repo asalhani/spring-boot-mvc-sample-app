78f286d88b877b174e28f47635e7a42de5d79049
adeeb <asalhani@outlook.com>
2019-08-17 13:27:06 +0300

    Initial application generated by JHipster-6.2.0

---
+++ jBlog-app/src/main/java/com/app/jblog/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.app.jblog.repository;
+
+import com.app.jblog.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
--- tasks-jhipster/src/main/java/com/app/tasks/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.app.tasks.service.dto;
